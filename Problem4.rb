846defe68594e8c2ce13956eb1964aa334a90b74
Andrew Lee <student@epicodus.com>
2015-05-31 16:57:42 -0700

    will find the largest palindrome of two 3 digit numbers (the product)

---
+++ Problem4.rb
@@ -0,0 +1,26 @@
+#Making an array with all the possible combinations
+numberset = Array(100..999)
+totalset = numberset.product(numberset)
+timetodo = totalset.length
+setarray = []
+n = 0
+while n < timetodo
+  result = totalset[n]
+  answer = result[0] * result[1]
+  setarray.push(answer)
+  n += 1
+end
+
+#look through and see which are palindromes
+holdingarray = []
+setarray.each do |num|
+  chknum = num.to_s
+  revchknum = chknum.reverse
+  if chknum == revchknum
+    holdingarray.push(num)
+  end
+  holdingarray
+end
+
+#sort and find which is the largest by looking at the last one
+holdingarray.sort
